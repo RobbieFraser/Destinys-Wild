371e6f703125000451a805bab3590a7952ebc737
Robbie <robbiefraser@hotmail.com>
2015-09-24 23:41:56 +1200

    Packed images into data/images

--- SWEN 222 Project/src/Renderer/TestFrame.java
+++ SWEN 222 Project/src/Renderer/TestFrame.java
@@ -8,8 +8,8 @@ import javax.swing.JFrame;
 
 public class TestFrame extends JFrame {
 	
-	//GameImagePanel gamePanel = new GameImagePanel();
-	LevelEditorPanel gamePanel = new LevelEditorPanel();
+	GameImagePanel gamePanel = new GameImagePanel();
+	//LevelEditorPanel gamePanel = new LevelEditorPanel();
 	
 	public TestFrame(){
 		super("Test Renderer");
