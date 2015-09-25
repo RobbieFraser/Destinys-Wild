20509f3c6ba7308e3e1ac01907d02d2b3e869fc9
pangaaro <pangaaro@tasting-room.ecs.vuw.ac.nz>
2015-09-25 15:32:02 +1200

    Added things to Master in CS

--- SWEN 222 Project/src/clientServer/Slave.java
+++ SWEN 222 Project/src/clientServer/Slave.java
@@ -1,9 +1,31 @@
 package clientServer;
 
+import game.Board;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
 
 public final class Slave extends Thread implements KeyListener  {
+
+	private final Socket socket;
+	private Board game;
+	private DataOutputStream output;
+	private DataInputStream input;
+	private int uid;
+	private int totalSent;
+
+
+	/**
+	 * Similar to Pacman. Slave doesn't actually process any computation, other
+	 * than updating the display and refreshing it when it receives data
+	 * from the master connection.
+	 */
+	public Slave(Socket socket) {
+		this.socket = socket;
+	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
