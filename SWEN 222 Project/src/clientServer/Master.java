f963b63b3f45a9473b092cbe384a12a475e7fcac
thepangman <azzapang40@yahoo.com>
2015-09-25 15:30:17 +1200

    Added some things into Master in C/S

--- SWEN 222 Project/src/clientServer/Master.java
+++ SWEN 222 Project/src/clientServer/Master.java
@@ -1,5 +1,21 @@
 package clientServer;
 
+import java.net.Socket;
+
+import game.Board;
+
 public final class Master extends Thread {
+
+	private final Board board;
+	private final int broadcastClock;
+	private final int playerID;
+	private final Socket socket;
+
+	public Master(Socket socket, int playerID, int broadcastClock, Board board) {
+		this.board = board;
+		this.broadcastClock = broadcastClock;
+		this.socket = socket;
+		this.playerID = playerID;
+	}
 
 }
