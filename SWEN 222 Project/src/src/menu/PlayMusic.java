aaa90f97650ee6355b5bfd63ec6c3754c2666914
DanielYska <d.yska249@gmail.com>
2015-09-25 21:17:06 +1200

    Commiting

--- SWEN 222 Project/src/src/menu/PlayMusic.java
+++ SWEN 222 Project/src/src/menu/PlayMusic.java
@@ -2,15 +2,16 @@ package menu;
 
 import java.io.FileInputStream;
 
-import javazoom.jl.decoder.JavaLayerException;
-import javazoom.jl.player.Player;
+//import javazoom.jlme.decoder.JavaLayerException;
+import javazoom.jlme.decoder.*;
+import javazoom.jlme.util.Player;
 
 public class PlayMusic {
 	private final String MEDIA_PATH = "data/music/";
 	private FileInputStream musicInputStream;
 	private Player playMP3;
 
-	public synchronized void playSound(String fileName) {
+	public synchronized void playSound(final String fileName) {
 		new Thread(new Runnable() {
 			public void run() {
 				try {
@@ -37,15 +38,15 @@ public class PlayMusic {
 	public void startPlaying(){
 		try {
 			playMP3.play();
-		} catch (JavaLayerException e) {
+		} catch (Exception e) {
+
+//		} catch (JavaLayerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
 	public void stopPlaying(){
-		playMP3.close();
+		//playMP3.close();
 	}
-
-
 }
