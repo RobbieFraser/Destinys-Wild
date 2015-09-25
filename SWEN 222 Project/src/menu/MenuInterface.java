1f64899a7ede00e622250c0edc1b57e3bdfd3b5d
Daniel Yska <d.yska249@gmail.com>
2015-09-25 12:15:03 +1200

    Moved board around

--- SWEN 222 Project/src/menu/MenuInterface.java
+++ SWEN 222 Project/src/menu/MenuInterface.java
@@ -5,16 +5,12 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 import javax.swing.AbstractAction;
-import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -22,9 +18,9 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 import javax.swing.border.Border;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class MenuInterface {
 	private JFrame frame;
@@ -78,7 +74,10 @@ public class MenuInterface {
 		JButton btnNewGame = new JButton(newGameImage);
 		btnNewGame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("New Game button pressed");
+				//Set up text field
+			    JTextField nameInput = new JTextField(30);
+			    nameInput.setBounds(430, 13, 200, 20);
+				frame.getContentPane().add(nameInput);
 			}
 		});
 		btnNewGame.setBounds(25, 30, 150, 80);
