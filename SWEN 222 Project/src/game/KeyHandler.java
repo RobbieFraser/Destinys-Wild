30159764f23a6a3d5b856cd2ad6722b26c6cbd43
thepangman <azzapang40@yahoo.com>
2015-09-27 10:10:45 +1300

    Created a KeyHandler and WindowHandler class to take in relevant inputs within the game package

---
+++ SWEN 222 Project/src/game/KeyHandler.java
@@ -0,0 +1,26 @@
+package game;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
