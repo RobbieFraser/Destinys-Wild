268a6e48ead6d66b65a35374f24d0e9445d2c1bc
thepangman <azzapang40@yahoo.com>
2015-10-09 12:03:04 +1300

    Minor update

--- SWEN 222 Project/src/clientServer/GameServer.java
+++ SWEN 222 Project/src/clientServer/GameServer.java
@@ -208,7 +208,7 @@ public class GameServer extends Thread {
 				player.setEast(intToBool(packet.getEast()));
 				player.setWest(intToBool(packet.getWest()));
 				player.setSouth(intToBool(packet.getSouth()));
-				player.updateWalkState(packet.getWalkstate());
+				player.updateWalkCycle();
 				packet.writeData(this);
 			}
 		} catch (IndexOutOfBoundsException e) {
